f3ac27b76f6d08aafec40633e310ec89a48ccd80
ManasaPola <6694manasa@gmail.com>
2019-04-24 20:18:26 -0700

    code

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ pip3 install efficient-apriori
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "$SCRIPT_DIR"
 cd  "${SCRIPT_DIR}/Code"
-python3 aprior.py
+python3 apriori.py
 
 pip3 install pickle
 pip3 install numpy
