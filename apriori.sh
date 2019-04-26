3ba061456d33102868a75a079ed482de33dcb146
ManasaPola <6694manasa@gmail.com>
2019-04-25 21:44:56 -0700

    modified added few more files

--- apriori.sh
+++ apriori.sh
@@ -4,8 +4,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "$SCRIPT_DIR"
 cd  "${SCRIPT_DIR}/Code"
 
-
-sudo apt-get -y install r-base
 RScript aprioriAlgo.r
 
 echo "Go to Evals folder to see the results"
